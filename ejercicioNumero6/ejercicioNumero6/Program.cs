36cd0992708d93fadf3659d1205fb46b53c6b7bf
Mayerli2003 <87994175+Mayerli2003@users.noreply.github.com>
2022-02-11 17:30:01 -0600

    arregloVistoEnClases

--- ejercicioNumero6/ejercicioNumero6/Program.cs
+++ ejercicioNumero6/ejercicioNumero6/Program.cs
@@ -6,22 +6,24 @@ namespace ejercicioNumero6
     {
         static void Main(string[] args)
         {
-            
+
             int A;
             Console.WriteLine("Ingrese un valor entero para A");
             A = int.Parse(Console.ReadLine());
 
-            int B ;
+            int B;
             Console.WriteLine("Ingrese un valor entero para B");
             B = int.Parse(Console.ReadLine());
 
-            int C ;
+            int C;
             Console.WriteLine("Ingrese un valor entero para C");
             C = int.Parse(Console.ReadLine());
 
-            int D ;
+            int D;
             Console.WriteLine("Ingrese un valor entero para D");
             D = int.Parse(Console.ReadLine());
+
+            int e;
 
             Console.WriteLine("Valores iniciales");
 
@@ -29,13 +31,20 @@ namespace ejercicioNumero6
             Console.WriteLine("B=" + B);
             Console.WriteLine("C=" + C);
             Console.WriteLine("D=" + D);
+            // Tomar los valores de las otras variables
+            e = B;
+            B = C;
+            C = A;
+            A = D;
+            D = e;
+
 
             Console.WriteLine("Valores finales");
 
-            Console.WriteLine(" B toma el valor de C → B=" + C);
-            Console.WriteLine(" C toma el valor de A → C=" + A);
-            Console.WriteLine(" A toma el valor de D → A=" + D);
-            Console.WriteLine(" D toma el valor de B → D=" + B);
+            Console.WriteLine(" B toma el valor de C → B=" + B);
+            Console.WriteLine(" C toma el valor de A → C=" + C);
+            Console.WriteLine(" A toma el valor de D → A=" + A);
+            Console.WriteLine(" D toma el valor de B → D=" + D);
         }
         
     }
